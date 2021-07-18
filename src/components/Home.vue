fb5160f18951784a3264797628314bc31f621166
Ricardo <francoliricardo@gmail.com>
2021-07-18 20:20:07 +0200

    create simple grid, iframes with 16:9 aspect ratio

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,21 @@
 <template>
-	<div v-for="video in videos.data" :key="video.id">
-		<iframe
-			:id="video.id"
-			:src="`${video.url}?fs=0&modestbranding=1&playsinline=1&rel=0`"
-			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-			width="640"
-			height="360"
-			frameborder="0"
-		></iframe>
+	<div class="grid gap-y-3">
+		<div
+			v-for="video in videos.data"
+			:key="video.id"
+			class="relative w-screen"
+			style="padding-top: 56.25%"
+		>
+			<iframe
+				:id="video.id"
+				:src="`${video.url}?fs=0&modestbranding=1&playsinline=1&rel=0`"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+				frameborder="0"
+				class="absolute top-0 left-0 w-full h-full"
+			>
+				></iframe
+			>
+		</div>
 	</div>
 </template>
 
@@ -21,4 +29,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+iframe {
+	min-width: 321px;
+}
+</style>
