0c11265df5929ce4dcb57e71474127e95a4da833
Ricardo <francoliricardo@gmail.com>
2021-07-18 18:51:19 +0200

    add basic components to display videos

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <Home msg="Hello Vue 3 + Vite" />
+	<Home />
 </template>
 
-<script setup>
-import Home from './components/Home.vue'
+<script>
+import Home from './components/Home.vue';
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+export default {
+	components: {
+		Home,
+	},
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,24 @@
 <template>
-<p>
-  Hello everyone!
-</p>
+	<div v-for="video in videos.data" :key="video.id">
+		<iframe
+			:id="video.id"
+			:src="`${video.url}?fs=0&modestbranding=1&playsinline=1&rel=0`"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			width="640"
+			height="360"
+			frameborder="0"
+		></iframe>
+	</div>
 </template>
 
-<script setup>
-import { defineProps, reactive } from 'vue'
+<script>
+import videos from '../../data/videos.json';
 
+export default {
+	data: () => ({
+		videos,
+	}),
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
