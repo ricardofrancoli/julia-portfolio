3c5dc3596302e5705a02cf84798234e22abedca9
Ricardo <francoliricardo@gmail.com>
2021-07-18 23:09:52 +0200

    add info bar (top navbar)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-	<Home />
+	<div>
+		<InfoBar />
+		<Home />
+	</div>
 </template>
 
 <script>
+import InfoBar from './components/InfoBar.vue';
 import Home from './components/Home.vue';
 
 export default {
 	components: {
+		InfoBar,
 		Home,
 	},
 };
---
+++ src/components/InfoBar.vue
@@ -0,0 +1,14 @@
+<template>
+	<header id="info-bar" class="mx-6 h-40">
+		<nav class="info-title-container fixed top-10 z-10">
+			<h1 class="info-title text-5xl font-semibold">Júlia García</h1>
+			<h2 class="text-2xl">1st Assistant Director</h2>
+		</nav>
+	</header>
+</template>
+
+<script>
+export default {
+	setup() {},
+};
+</script>
